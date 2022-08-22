f0a4d65f1051eb31f11d3d9ea16796427829ee44
nstegwart <gian.devx@gmail.com>
2022-08-22 13:45:12 +0700

    Define your audience section

--- src/pages/add-campaign/index.js
+++ src/pages/add-campaign/index.js
@@ -33,7 +33,7 @@ export default function PageOne() {
           <img src={campaignIcon} alt="campaign" />
         </div>
         <div className={styles.ctnMidInput}>
-          <Typography variant="subtitle1" paragraph>
+          <Typography variant="h6" paragraph>
             Campaign Name
           </Typography>
           <div className={styles.ctnGray}>
@@ -41,7 +41,7 @@ export default function PageOne() {
           </div>
         </div>
         <div className={styles.ctnRightInput}>
-          <Typography variant="subtitle1" paragraph>
+          <Typography variant="h6" paragraph>
             Start Date
           </Typography>
           <div className={styles.ctnDate}>
@@ -61,7 +61,7 @@ export default function PageOne() {
           <img src={calendarIcon} alt="campaign" />
         </div>
         <div className={styles.ctnMidInput}>
-          <Typography variant="subtitle1" paragraph sx={{ marginBottom: 0 }}>
+          <Typography variant="h6" paragraph sx={{ marginBottom: 0 }}>
             Availability
           </Typography>
           <Typography variant="span" paragraph>
@@ -90,8 +90,115 @@ export default function PageOne() {
                 <img src={blackCalendar} alt="calendar" />
               </div>
             </div>
+            <div className={styles.inputGray}>
+              <div className={styles.leftWrapper}>
+                <img src={checklistIcon} alt="checklist" />
+                <span>Never</span>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
+    )
+  }
+
+  function renderTargeting(){
+    return (
+      <div className={styles.ctnSectionTarget}>
+        <div className={styles.ctnIconTarget}>
+          <img src={targetIcon} alt="campaign" />
+        </div>
+        <div className={styles.ctnMidInput}>
+          <Typography variant="h6" paragraph>
+            Targeting
+          </Typography>
+          <Typography variant="span" paragraph>
+            Reach exactly the Crypto-Users that you want to reach by using our state-of-the-art targeting options. And no need to worry – even if your audiences overlap, we will make sure that each wallet only receives your wallet ad once to get the most out of your budget and to avoid that your project might be considered as spam.
+          </Typography>
+          </div>
+      </div>
+    )
+  }
+
+  function renderBudget(){
+    return (
+      <div className={styles.ctnSectionTarget}>
+        <div className={styles.ctnIconTarget}>
+          <img src={totalBudgetIcon} alt="campaign" />
+        </div>
+        <div className={styles.ctnInputBudget}>
+          <div className={styles.ctnTotal}>
+            <Typography variant="subtitle1" fontSize={20} marginBottom={1} paragraph>
+              Total Budget
+            </Typography>
+            <Typography variant="subtitle1" fontSize={20} color={'#667C8B'} marginBottom={1} paragraph>
+              USD1,000
+            </Typography>
+          </div>
+          {/* <div className={styles.ctnHorizontalRow} /> */}
+          <div className={styles.ctnDesc}>
+            <Typography variant="subtitle1" fontSize={20} marginBottom={1} paragraph>
+            That's great!
+            </Typography>
+            <Typography variant="subtitle1" fontSize={20} marginBottom={1} paragraph>
+            <b>100,000 users</b> will receive your airdrop
+            </Typography>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  function renderCardAudience(){
+    return (
+      <div className={styles.ctnAudience}>
+        <div className={styles.ctnTitle}>
+          <div className={styles.rowTitle} />
+          <Typography variant="h5" marginTop={2} marginX={2} paragraph>
+            Define your audiences
+          </Typography>
+          <div className={styles.rowTitle} />
+        </div>
+        <div className={styles.cardAudience}>
+          <div className={styles.headerAudience}>
+            <Typography variant="h5" textAlign={'center'} color={'#fff'}>
+              Audience 1:
+            </Typography>
+          </div>
+          <div className={styles.ctnDescAudience}>
+            <Typography variant="span" textAlign={'center'} paragraph>
+            The audience consists of a broad mix of users, optimized by our algorithm.
+            </Typography>
+            <div className={styles.ctnPrice}>
+              <Typography variant="h5" textAlign={'center'} >
+                USD500
+              </Typography>
+            </div>
+            <Typography variant="span" textAlign={'center'} paragraph>
+              USD1 per airdrop
+            </Typography>
+            <div className={styles.ctnAmount}>
+            <Typography variant="h6" color={'#7589FA'} textAlign={'center'}>
+              8,333 users
+            </Typography>
+            <Typography variant="span" textAlign={'center'} paragraph>
+              In this audience will receive airdrops
+            </Typography>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    )
+  }
+
+  function renderDefineAudience(){
+    return (
+      <div className={styles.ctnDefineAudience}>
+        {renderTargeting()}
+        {renderCardAudience()}
+        {renderBudget()}
+        {/* {} */}
       </div>
     )
   }
@@ -100,7 +207,7 @@ export default function PageOne() {
     <div className={styles.ctnRoot}>
       {renderCampaignName()}
       {renderAvailability()}
-      {/* {} */}
+      {renderDefineAudience()}
     </div>
   );
 }
--- src/pages/add-campaign/styles.js
+++ src/pages/add-campaign/styles.js
@@ -102,6 +102,106 @@ const useStyles = makeStyles((theme) => ({
             objectFit :'contain',
             marginLeft: 14
         }
+    },
+    ctnDefineAudience: {
+        background: 'rgb(45, 49, 151, 0.1)',
+        marginLeft: 20,
+        marginRight: 20,
+        paddingTop: 20,
+        paddingBottom: 20,
+        borderRadius: 20
+    },
+
+    ctnSectionTarget: {
+        display: 'flex',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        boxShadow: '0px 10px 20px rgba(114, 125, 196, 0.5)',
+        marginLeft: 20,
+        marginRight: 20,
+        marginBottom: 20,
+        padding: 20,
+        borderRadius: 20
+    },
+    ctnIconTarget: {
+        marginRight: 40,
+        '& > img': {
+            width: 100,
+            height: 100,
+            objectFit: 'contain'
+        }
+    },
+    ctnInputBudget: {
+        display :'flex',
+        alignItems: 'center',
+        height: '100%'
+    },
+    ctnTotal: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        borderRight: '1px solid #667C8B',
+        paddingRight: 120,
+        marginRight: 120
+    },
+    ctnHorizontalRow: {
+        marginLeft: 120,
+        marginRight: 120,
+        backgroundColor: '#000',
+        width: 1,
+        height: '100%'
+    },
+    ctnDesc: {
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    ctnAudience: {
+        position: 'relative'
+    },
+    ctnTitle: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    rowTitle: {
+        height: '2px',
+        background: '#fff',
+        flexGrow: 1,
+    },
+    cardAudience: {
+        backgroundColor: '#fff',
+        boxShadow: '0px 10px 20px rgba(114, 125, 196, 0.5)',
+        width: 280,
+        marginLeft: 20,
+        padding: 20,
+        borderRadius: 20,
+        marginBottom: 40,
+        paddingTop: 0,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+    },
+    headerAudience: {
+        backgroundColor: '#7589FA',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '200px',
+        paddingTop: 12,
+        paddingBottom: 12,
+        borderBottomLeftRadius: 50,
+        borderBottomRightRadius: 50,
+    },
+    ctnDescAudience: {
+        marginTop: 20
+    },
+    ctnPrice: {
+        width: '100%',
+        border: '1px solid #122D46',
+        paddingTop: 12,
+        paddingBottom: 12,
+        borderRadius: 12
     }
 }))
 
