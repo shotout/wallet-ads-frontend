d97d7c715fb136759b3520e6664e436589d82d3e
nstegwart <gian.devx@gmail.com>
2022-09-11 14:22:56 +0700

    Fixing import object question

--- src/pages/create-campaign/index.js
+++ src/pages/create-campaign/index.js
@@ -18,7 +18,27 @@ import { getCampaignItem, handleAddCampaign } from '../../utils/requests';
 import DefaultButton from '../../components/default-button';
 import moment from 'moment';
 import AuthFooter from '../../components/auth-footer';
-import { bannerText, questionObj } from './questioMarkPopup';
+
+const questionObj = {
+  availability: "To keep the users' wallets clean and to deliver high-quality advertisement, the ad will be auto-deletes from the users' wallets after a certain amount of time. ",
+  cryptocurrencies_used: "Target users that own specific cryptocurrencies. If you select several cryptocurrencies, you will target all users that own at least one of the chosen cryptocurrencies.",
+  account_age: "Target users whose cryptowallet accounts already exist for a certain amount of time.",
+  available_credit_in_wallet: "Target users who have a certain amount of credit in their wallet (daily updated equivalent in USD)",
+  trading_volume: "Target users who already traded assets and cryptocurrencies of a certain trading volume.",
+  airdrops_received: "Target users who already received a certain amount of airdrops.",
+  amount_of_transaction: "Target users who made a certain amount of transactions within a certain timeframe.",
+  nft_Purchases: "Target users who made purchases of a certain Creator or NFT. You can freely add several creators or NFTs here by separating them with a comma.",
+  upload_audience: "If you have a list of wallet addresses that you would like to target, you can upload it here. File format: .xlsx or .csv - max. file size 8mb.",
+  collection_page_text: "Add a text for your collection page to describe what it is about.",
+  add_social_media_link: "On your collection page, you can link to your social media pages. If you do not have an account on one of the pages, just leave the field empty.",
+  ad_name: "This is the name of your advertisement.",
+  media: "Upload an ad image. This will become the NFT that will be sent to the users. Format: png/jpg ",
+  ad_text: "This will be the description that shows along with your wallet ad.",
+  choose_audience: "If you create several audiences and several ads, you can decide here which of your audiences should see which ad.",
+  collection_page_name: "Name of the Collection page under which your ad will be listed. This could be your brand name or artist name.",
+  logo_text: 'Upload a banner for the collection page. Recommended size: 350x350px Format: png/jpg.',
+  logo_text_banner: 'Upload a banner for the collection page. Recommended size: 400x350px Format: png/jpg.',
+}
 
 // ----------------------------------------------------------------------
 
@@ -754,7 +774,7 @@ export default function AddCampaign({ content }) {
                 onMouseLeave={handleHoverClose}
                 src={askIcon}
                 alt="ask" />
-              {renderPopover('logo_text',bannerText('350x350px'))}
+              {renderPopover('logo_text',questionObj.logo_text)}
             </div>
             <Typography variant="body2"  color='#808080'>
               Recommended size 350x350px 
@@ -769,11 +789,11 @@ export default function AddCampaign({ content }) {
                   Add banner
                 </Typography>
               <img
-                onMouseEnter={(event) => {handleHoverOpen(event, 'logo_text')}}
+                onMouseEnter={(event) => {handleHoverOpen(event, 'logo_text_banner')}}
                 onMouseLeave={handleHoverClose}
                 src={askIcon}
                 alt="ask" />
-              {renderPopover('logo_text',bannerText('400x350px'))}
+              {renderPopover('logo_text_banner',questionObj.logo_text_banner)}
             </div>
             <Typography variant="body2"  color='#808080'>
               Recommended size 1400x350px 
