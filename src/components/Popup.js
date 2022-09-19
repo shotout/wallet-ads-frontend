7380cfd52983054814ed1d90763331e846857155
Jun al-Fajr <jun.fajr@gmail.com>
2022-09-19 22:43:59 +0700

    edit style cookiee popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,21 +5,31 @@ const Popup = () => {
     <div>
       <CookieConsent
         location="bottom"
-        buttonText="Accept!!"
-        cookieName="myAwesomeCookieName2"
-        // style={{ background: '#E4E6FA' }}
-        style={{ background: 'linear-gradient(90deg, #E691F3 17.5%, #7089FF 82.5%)' }}
+        // enableDeclineButton
+        buttonText="Accept"
+        declineButtonText="Cookie Settings"
+        cookieName="cookies"
+        style={{ background: 'black' }}
         buttonStyle={{
-          // background: 'blue',
-          // background: 'linear-gradient(to left, orange , yellow, green, cyan, blue, violet)',
+          background: 'white',
           color: '#4e503b',
-          fontSize: '14px',
+          fontSize: '12px',
           fontWeight: 'bold',
         }}
         expires={150}
       >
-        This website uses cookies to enhance the user experience.{' '}
-        {/* <span style={{ fontSize: '10px' }}>This bit of text is smaller :O</span> */}
+        We use cookies and similar technologies to enable services and functionality on our site and to understand your
+        interaction with our service By clicking on accept, you agree to our use of such technologies for marketing and
+        analytic.{' '}
+        <span>
+          <a href="/cookies" style={{ color: 'white' }}>
+            See Privacy Policy
+          </a>
+        </span>
+        <span>
+          {/* <button buttonText>First child button</button> */}
+          {/* <button>second child button</button> */}
+        </span>
       </CookieConsent>
     </div>
   );
