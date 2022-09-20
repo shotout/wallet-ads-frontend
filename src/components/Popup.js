308cadd41b23093559740027844f736042af9fb3
nstegwart <gian.devx@gmail.com>
2022-09-20 15:11:10 +0700

    Example cookie popover

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,7 +1,38 @@
-import CookieConsent, { Cookies } from 'react-cookie-consent';
+import CookieConsent, { Cookies,  } from 'react-cookie-consent';
 import Link from 'next/link';
+import useStyles from './PopupStyle';
+import { useState } from 'react';
+import { Popover } from '@mui/material';
 
 const Popup = () => {
+  const [showPopover, setShowPopover] = useState(false)
+
+  const styles = useStyles()
+
+  function renderPopover(){
+    return (
+      <Popover
+          id={"cookie-menu"}
+          open={Boolean(showPopover)}
+          anchorEl={"menu-cookie"}
+          anchorOrigin={{
+            vertical: 'center',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'center',
+            horizontal: 'center',
+          }}
+          onClose={() => {setShowPopover(false)}}
+          className={styles.ctnPopover}
+      >
+        <div className={styles.ctnContentPopup}>
+          <span>Popup Showed</span>
+        </div>
+        </Popover>
+    )
+  }
+
   return (
     <div>
       <CookieConsent
@@ -11,29 +42,39 @@ const Popup = () => {
         // declineButtonText="Cookie Settings"
         cookieName="cookies"
         style={{ background: 'black' }}
+        contentStyle={{ height: '100%'}}
+        styl
         buttonStyle={{
-          background: 'white',
-          color: '#4e503b',
-          fontSize: '12px',
-          fontWeight: 'bold',
+          width: 120,
+          height: 40,
+          border: '1px solid #fff',
+          background: '#000',
+          color: '#fff',
+          marginTop: 20,
         }}
         expires={150}
       >
-        We use cookies and similar technologies to enable services and functionality on our site and to understand your
-        interaction with our service By clicking on accept, you agree to our use of such technologies for marketing and
-        analytic.{' '}
-        <span>
-          <Link href="/cookies">
-            <a style={{ color: 'white' }}>See Privacy Policy</a>
-          </Link>
-        </span>
-        {/* <span>
-          <button buttonText>First child button</button>
-          <button>second child button</button>
-        </span> */}
+        <div className={styles.ctnRoot}>
+          <div className={styles.ctnText}>
+            We use cookies and similar technologies to enable services and functionality on our site and to understand your
+            interaction with our service By clicking on accept, you agree to our use of such technologies for marketing and
+            analytic.{' '}
+            <span>
+              <Link href="/cookies">
+                <a style={{ color: 'white' }}>See Privacy Policy</a>
+              </Link>
+            </span>
+          </div>
+          <div className={styles.ctnBtn}>
+            <div className={styles.btnManage} onClick={() => { setShowPopover(true)}}>
+              <span>Manage Cookie</span>
+            </div>
+          </div>
+        </div>
       </CookieConsent>
+      {renderPopover()}
     </div>
-  );
+  )
 };
 
 export default Popup;
