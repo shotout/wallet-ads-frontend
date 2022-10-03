bd2a7e0129cc8ddd13be20b3ee56391383aa8bed
Zul Amri Durrin Nafis <b2b-adira@contractor.bukalapak.com>
2022-10-04 01:01:09 +0700

    Change route to wallet ads

--- src/pages/forbidden/index.js
+++ src/pages/forbidden/index.js
@@ -17,7 +17,7 @@ export default function NoMobile() {
   function renderHeader() {
     return (
       <div className={styles.ctnHeader}>
-        <Link href={routes.walletadsMain}>
+        <Link href={routes.walletads}>
           <img src={appIcon} alt="wallet-ads" />
         </Link>
         <div className="menu">
@@ -30,7 +30,7 @@ export default function NoMobile() {
   const renderMenu = () => (
     <div className={styles.ctnMenuRoot}>
       <div className={styles.ctnHeader}>
-        <Link href={routes.walletadsMain}>
+        <Link href={routes.walletads}>
           <img src={appIcon} alt="wallet-ads" />
         </Link>
         <div className="menu">
