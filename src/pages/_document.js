3001ccf8515b786286a34f0fab98c567bdd8d6e6
Zul Amri Durrin Nafis <b2b-adira@contractor.bukalapak.com>
2022-10-14 12:30:18 +0700

    Add google ads

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -42,6 +42,16 @@ export default class MyDocument extends Document {
         </Head>
 
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-MQGQLVG"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            />`,
+            }}
+          />
           <Main />
           <NextScript />
         </body>
