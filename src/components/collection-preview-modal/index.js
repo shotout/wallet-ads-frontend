c730d1b7dd4834f5a95fb5d148051b26da6fb1a8
Zul Amri Durrin Nafis <b2b-adira@contractor.bukalapak.com>
2022-11-13 12:00:17 +0700

    Fixing text align

--- src/components/collection-preview-modal/index.js
+++ src/components/collection-preview-modal/index.js
@@ -58,45 +58,11 @@ export default function CollectionPreview({ label, bannerSource, logoSource, for
             {/* {formValues?.ads_page_telegram && <div className={styles.ctnSocial}>|</div>} */}
           </div>
         </div>
-
-        {/* <Grid style={{ width: '40%', paddingLeft: 50 }}>
-          <Typography fontSize={20} fontWeight={700} marginBottom={3}>
-            Your social media links:
+        <div style={{ paddingRight: 20 }}>
+          <Typography variant="body1" textAlign={'justify'}>
+            {formValues.ads_page_description || 'Your collection page text here'}
           </Typography>
-          {formValues?.ads_page_website && (
-            <div className={styles.ctnSocial}>
-              <img src={websiteIcon} alt="discord" />
-              <Typography fontSize={15} fontWeight={400}>
-                {formValues?.ads_page_website}
-              </Typography>
-            </div>
-          )}
-          {formValues?.ads_page_discord && (
-            <div className={styles.ctnSocial}>
-              <img src={discordIcon} alt="discord" />
-              <Typography fontSize={15} fontWeight={400}>
-                {formValues?.ads_page_discord}
-              </Typography>
-            </div>
-          )}
-          {formValues?.ads_page_medium && (
-            <div className={styles.ctnSocial}>
-              <img src={mediumIcon} alt="discord" />
-              <Typography fontSize={15} fontWeight={400}>
-                {formValues?.ads_page_medium}
-              </Typography>
-            </div>
-          )}
-          {formValues?.ads_page_telegram && (
-            <div className={styles.ctnSocial}>
-              <img src={telegramIcon} alt="discord" />
-              <Typography fontSize={15} fontWeight={400}>
-                {formValues?.ads_page_telegram}
-              </Typography>
-            </div>
-          )}
-        </Grid> */}
-        <Typography variant="body1">{formValues.ads_page_description || 'Your collection page text here'}</Typography>
+        </div>
       </div>
     );
   }
