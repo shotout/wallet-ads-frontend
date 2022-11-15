f8e56a7602b04dbdc8b4162f7c2f30dba60b8cb0
Zul Amri Durrin Nafis <b2b-adira@contractor.bukalapak.com>
2022-11-15 14:30:42 +0700

    Change Label on page preview

--- src/components/collection-preview/index.js
+++ src/components/collection-preview/index.js
@@ -2,49 +2,47 @@ import { Typography } from '@mui/material';
 import React from 'react';
 import useStyles from './styles';
 
-export default function CollectionPreview({ label, bannerSource, logoSource, formValues }){
-    const styles = useStyles()
+export default function CollectionPreview({ label, bannerSource, logoSource, formValues }) {
+  const styles = useStyles();
 
-    function renderProfile(){
-        if(logoSource){
-            return (
-                <img src={logoSource} alt="logo" className={styles.logoImage} />
-            )
-        }
-        return (
-            <div className={styles.ctnLogo} />
-        )
+  function renderProfile() {
+    if (logoSource) {
+      return <img src={logoSource} alt="logo" className={styles.logoImage} />;
     }
+    return <div className={styles.ctnLogo} />;
+  }
 
-    function renderBanner(){
-        return (
-            <div className={styles.ctnBanner}>
-                <div className={styles.bannerItem}>
-                    {bannerSource ? (
-                        <img src={bannerSource} alt="banner-source" />
-                    ): (
-                        <Typography variant="h6" color={'#fff'}>Banner image 1400 x 400</Typography>
-                    )}
-                </div>
-
-            {renderProfile()}
-            </div>
-        )
-    }
-
-    function renderDesc(){
-        return (
-            <div className={styles.ctnDesc}>
-                <Typography variant="h6">{formValues.ads_page_name || 'Collection page name'}</Typography>
-                <Typography variant="body1">{formValues.ads_page_description || 'Your collection page text here'}</Typography>
-            </div>
-        )
-    }
-
+  function renderBanner() {
     return (
-        <div className={styles.ctnRoot}>
-            {renderBanner()}
-            {renderDesc()}
+      <div className={styles.ctnBanner}>
+        <div className={styles.bannerItem}>
+          {bannerSource ? (
+            <img src={bannerSource} alt="banner-source" />
+          ) : (
+            <Typography variant="h6" color={'#fff'}>
+              Banner image 1400 x 350
+            </Typography>
+          )}
         </div>
-    )
+
+        {renderProfile()}
+      </div>
+    );
+  }
+
+  function renderDesc() {
+    return (
+      <div className={styles.ctnDesc}>
+        <Typography variant="h6">{formValues.ads_page_name || 'Collection page name'}</Typography>
+        <Typography variant="body1">{formValues.ads_page_description || 'Your collection page text here'}</Typography>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.ctnRoot}>
+      {renderBanner()}
+      {renderDesc()}
+    </div>
+  );
 }
