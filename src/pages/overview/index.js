1c537c15256cfdd1eb95aec3b388741ad4a5c01e
Zul Amri Durrin Nafis <b2b-adira@contractor.bukalapak.com>
2022-11-15 16:34:12 +0700

    Change title

--- src/pages/overview/index.js
+++ src/pages/overview/index.js
@@ -205,10 +205,7 @@ export default function Overview({ content, listCampaign, paginations, ctx }) {
                   fontWeight={800}
                   fontSize={14}
                 >
-                  {/* {dataPopover?.title} */}
-                  There are many variations of passages of Lorem Ipsum available, but the majority have suffered
-                  alteration in some form, by injected humour, or randomised words which don't look even slightly
-                  believable. If you are going to use a passage of Lorem Ipsum,
+                  {dataPopover?.title}
                 </Typography>
               </div>
               <Divider />
