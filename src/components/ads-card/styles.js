0411bc6602cb8299ce5faeb580d53a0273c2d768
Zul Amri Durrin Nafis <b2b-adira@contractor.bukalapak.com>
2022-11-22 22:17:06 +0700

    Adjust ads card style

--- src/components/ads-card/styles.js
+++ src/components/ads-card/styles.js
@@ -63,4 +63,25 @@ export default makeStyles((theme) => ({
     backgroundColor: '#b2bec3',
     marginBottom: 10,
   },
+  audienceWrapper: {
+    maxHeight: 70,
+    minHeight: 70,
+    overflowY: 'auto',
+    paddingLeft: 2,
+    paddingRight: 10,
+    marginBottom: 15,
+    '&::-webkit-scrollbar': {
+      width: '0.9em',
+      backgroundColor: 'rgba(217, 217, 217, 0.5)',
+      borderRadius: 10,
+    },
+    '&::-webkit-scrollbar-track': {
+      boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+      webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: 'rgba(128, 128, 128, 0.7)',
+      borderRadius: 10,
+    },
+  },
 }));
