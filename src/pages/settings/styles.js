70691876e8a21c90f5ff2a0f14f04dafa4730627
vijayandika <vijayandika458@gmail.com>
2023-01-02 17:51:55 +0700

    Move some Text box in setting menu && Replacing text

--- src/pages/settings/styles.js
+++ src/pages/settings/styles.js
@@ -2,6 +2,19 @@ import { makeStyles } from '@mui/styles';
 import { NAVBAR } from '../../config';
 
 export default makeStyles((theme) => ({
+    ctnGridRadius: {
+        border: '1px solid #808080',
+        borderRadius: 12,
+        height: 34,
+        padding: 2,
+        paddingRight: 60,
+        paddingLeft: 60,
+        cursor: 'pointer',
+        marginTop: 10
+    },
+    changePassword: {
+        paddingTop: 32,
+    },
     ctnRoot: {
         position: 'relative',
         background: '#fff',
