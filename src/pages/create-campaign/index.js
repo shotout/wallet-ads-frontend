cffb547d760b1fec2574bca98450acf1a4400186
vijayandika <vijayandika458@gmail.com>
2023-01-05 15:23:36 +0700

    re

--- src/pages/create-campaign/index.js
+++ src/pages/create-campaign/index.js
@@ -786,7 +786,7 @@ export default function AddCampaign({ content, params }) {
         });
         if (isCampaignNameValid) {
           window.location.href = '#campaign-name';
-        } else if (isAudienceValid.length === 0 || isAudienceUnderMinimum.length > 0) {
+        } else if (isAudienceValid.length === 0 || isAudienceUnderMinimum.length > 0 && errorBox.errorAds || !isAudienceFormAdsValid) {
           window.location.href = '#card-audience';
         } else if (!isCollectionSection) {
           // formValues.ads_page_name && formValues.ads_page_description && logoCollection && bannerCollection;
