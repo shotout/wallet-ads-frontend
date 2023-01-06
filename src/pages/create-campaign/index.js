adaf883e7ef905ef709670ad7ab160ef51b91774
vijayandika <vijayandika458@gmail.com>
2023-01-06 17:58:40 +0700

    re

--- src/pages/create-campaign/index.js
+++ src/pages/create-campaign/index.js
@@ -1672,7 +1672,7 @@ export default function AddCampaign({ content, params }) {
               >
                 <CardAudience
                   isError={errorBox.errorAudience}
-                  isErrorAudienceNull={errorBox.errorAudienceNull || errorBox.errorAds && index > 0}
+                  isErrorAudienceNull={errorBox.errorAudienceNull || errorBox.errorAds}
                   onChangeBudget={(event) => {
                     handleChangeBudget(event, 'budgetAds', index);
                   }}
