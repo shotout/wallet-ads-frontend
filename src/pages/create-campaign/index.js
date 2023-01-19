fd6e02278f7c422ae3335c4076db77ecd4b07408
Vijay Andika <vijayandika@Vijays-MacBook-Pro.local>
2023-01-19 19:27:45 +0700

    re

--- src/pages/create-campaign/index.js
+++ src/pages/create-campaign/index.js
@@ -2443,8 +2443,8 @@ export default function AddCampaign({ content, params }) {
         ${errorBox.errorAds && content.description.some((desc) => desc.isErr) ? styles.ctnRedBorder : ''}`}
         key={content.adsId}
       >
-        {!isAdsArrValid2(content) && errorBox.errorAds ? (
-          <div className={`${errorBox.errorAds && !isAdsArrValid(content) ? styles.ctnAdsTitle : {}}`}>
+        {!isAdsArrValid2(content) ? (
+          <div className={`${!isAdsArrValid(content) ? styles.ctnAdsTitle : {}}`}>
             Please assign an at least 1 audience to this ad or delete this ad.
           </div>
         ) : null}
