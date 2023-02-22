9f4a917fb93b691599431e612fde0a2560ae8f82
vijay <vijayandika458@gmail.com>
2023-02-22 16:20:27 +0700

    err

--- src/pages/create-campaign/index.js
+++ src/pages/create-campaign/index.js
@@ -574,7 +574,8 @@ export default function AddCampaign({ userData, content, params }) {
 
       pictureData.forEach((ads, adsIndex) => {
         if (ads.id) formRes.append(`campaign_ads[${adsIndex}][id]`, ads.id);
-        if (ads.name) formRes.append(`campaign_ads[${adsIndex}][name]`, ads.name);
+        // if (ads.name) formRes.append(`campaign_ads[${adsIndex}][name]`, ads.name);
+        if (ads.headlines) formRes.append(`campaign_ads[${adsIndex}][headlines]`, JSON.stringify(ads.headlines));
         if (ads.description) formRes.append(`campaign_ads[${adsIndex}][description]`, JSON.stringify(ads.description));
         // if (ads.description) formRes.append(`campaign_ads[${adsIndex}][description]`, ads.description);
 
