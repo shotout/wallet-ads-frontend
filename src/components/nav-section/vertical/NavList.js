b366884a377a19644317eb10dbd5a2953311753f
Vijay Andika <vijayandika@Vijays-MacBook-Pro.local>
2023-03-09 22:58:44 +0700

    re

--- src/components/nav-section/vertical/NavList.js
+++ src/components/nav-section/vertical/NavList.js
@@ -27,7 +27,7 @@ export default function NavList({ data, depth, hasChildren, isCollapse = false }
     if (!hasChildren) {
       push(data.path);
     }
-    setOpen(!open);
+    setOpen(!open); 
     localStorage.removeItem('dataAfterSave');
   };
 
