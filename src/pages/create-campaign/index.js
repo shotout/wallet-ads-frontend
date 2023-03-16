1edb1d872aa2991d81f5681cc870057646926b2b
vijayandika <vijayandika458@gmail.com>
2023-03-17 01:17:40 +0700

    set cc

--- src/pages/create-campaign/index.js
+++ src/pages/create-campaign/index.js
@@ -285,8 +285,6 @@ export default function AddCampaign({ userData, content, params }) {
           setPaymentDetails('paymentDetailsNull');
         }
         setPaymentMethod(checkUser.data.payment.payment_method);
-        const res = await getPaymentCC();
-        setDataPaymentCC(res[0].data[0].client_secret);
 
         setTimeout(() => {
           setShowCreditCard(
